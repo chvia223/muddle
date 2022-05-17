5da0af91d6e7fd49c06955a0109c2fe9bcd07147
Christian Viazzo <via17001@byui.edu>
2022-05-17 11:17:22 -0600

    Initial Commit

---
+++ MuddleUI/MuddleUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuddleUIApp.swift
+//  MuddleUI
+//
+//  Created by Christian Viazzo on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct MuddleUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
