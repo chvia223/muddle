c73a75c041d00ae9eff2ca64610194333bcf885e
Christian Viazzo <via17001@byui.edu>
2022-05-22 23:21:09 -0600

    Restructured to pull data from Board class

--- MuddleUI/ContentView.swift
+++ MuddleUI/ContentView.swift
@@ -8,62 +8,45 @@
 
 import SwiftUI
 
-struct Word {
-    var Elements = [LetterBox]()
-}
 
 struct ContentView: View {
+    @State var gameBoard = GameBoard()
+    @State var singleLetter: String = "x"
+    
     var body: some View {
         VStack {
+            ForEach(gameBoard.board.indices, id: \.self) { i in
+                let row = gameBoard.board[i]
+                HStack {
+                    ForEach(row.indices, id: \.self) { j in
+                        LetterBox(displayedLetter: row[j].letter, boxColor: row[j].state.color)
+                    }
+                }
+            }
+            
+            Spacer()
+            
             HStack {
-                LetterBox(letter: "b", color: .red)
-                LetterBox()
-                LetterBox()
-                LetterBox()
-                LetterBox()
+                
             }
             
             HStack {
-                LetterBox()
-                LetterBox()
-                LetterBox()
-                LetterBox()
-                LetterBox()
+                
             }
             
             HStack {
-                LetterBox()
-                LetterBox()
-                LetterBox()
-                LetterBox()
-                LetterBox()
+                
             }
             
-            HStack {
-                LetterBox()
-                LetterBox()
-                LetterBox()
-                LetterBox()
-                LetterBox()
+            Button("Press Me") {
+                gameBoard = testChangeLetterOnCommit(gameBoard: gameBoard)
+                print(gameBoard.board[0][0].letter, gameBoard.board[0][1].letter, gameBoard.board[0][2].letter, gameBoard.board[0][3].letter)
+                singleLetter = "k"
             }
-            
-            HStack {
-                LetterBox()
-                LetterBox()
-                LetterBox()
-                LetterBox()
-                LetterBox()
-            }
-            
-            HStack {
-                LetterBox()
-                LetterBox()
-                LetterBox()
-                LetterBox()
-                LetterBox()
-            }
+            Text(singleLetter)
         }
         .padding()
+        
         
     }
 }
